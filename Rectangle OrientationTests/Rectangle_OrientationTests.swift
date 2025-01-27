c8c2c52d2524b6ede8825e96f604051a1dff70fc
Ilya <fekl13in@gmail.com>
2025-01-27 21:30:02 +0300

    Initial Commit

---
+++ Rectangle OrientationTests/Rectangle_OrientationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rectangle_OrientationTests.swift
+//  Rectangle OrientationTests
+//
+//  Created by Ilya Feklin on 27.01.2025.
+//
+
+import Testing
+@testable import Rectangle_Orientation
+
+struct Rectangle_OrientationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
